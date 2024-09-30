b81ceb94afe67fec11b2a89ca1b6c5333a21e0c0
Jan Mottl <mottja@delta-skola.cz>
2024-09-30 11:10:47 +0200

    kk

---
+++ 02/Proc_SudaLicha/Proc_SudaLicha/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Proc_SudaLicha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej mi číslo ");
+            int cislo = int.Parse(Console.ReadLine());
+            if (cislo == 0) //pro odchycení nuly hned na začátku podmínky
+            {
+                Console.WriteLine("Číslo je NULA!");
+            }
+            else if(cislo % 2 == 0) //modulo "%" – Zbytek po celočíselném dělení čísla se rovná 0
+            {
+                Console.WriteLine("Číslo je SUDÉ!");
+            } else
+            {
+                Console.WriteLine("Číslo je LICHÉ!");
+            }
+        }
+    }
+}
