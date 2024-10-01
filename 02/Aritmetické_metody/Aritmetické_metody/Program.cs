94068684f6800b98715e8023864267a8e6914cf1
Jan Mottl <mottja@delta-skola.cz>
2024-10-01 12:42:31 +0200

    kk

---
+++ 02/Aritmetické_metody/Aritmetické_metody/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Aritmetické_metody
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 5;
+
+            //Hledání vyššího a nižšího čísla pomocí metody;
+            Console.WriteLine(Math.Min(x, y));
+            Console.WriteLine(Math.Max(x, y));
+
+            //Odmocnina (druhá odmocnina)
+            Console.WriteLine(Math.Sqrt(25));
+
+            //Mocnina – první číslo je mocněné druhým
+            Console.WriteLine(Math.Pow(5,2)); //5 na 2
+
+            //Absolutní hodnota
+            Console.WriteLine(Math.Abs(-20));
+
+            //Zaokrouhlení
+            Console.WriteLine(Math.Round(2.4));
+
+            //PI
+            Console.WriteLine(Math.PI);
+        }
+    }
+}
---
+++ 02/Práce_String/Práce_String/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Práce_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "ABCDEFGijk C";
+
+            //string.Lenght vrací délku daného řetězce
+            Console.WriteLine(a.Length);
+
+            //Výpis stringu jenom ve velkých, či malých písmenech
+            Console.WriteLine(a.ToUpper());
+            Console.WriteLine(a.ToLower());
+
+            //Indexování znaků ve stringu
+            Console.WriteLine(a[1]); //ve stringu "a" na indexu 1 (tzn. znak B)
+            Console.WriteLine(a[0]); //ve stringu "a" na indexu 0 (tzn. znak A)
+            Console.WriteLine(a[10]); //ve stringu "a" na indexu 10 (tzn. znak " ")
+
+            //lze takto převést string na konkrétní char
+            char pismeno = a[0];
+            Console.WriteLine(pismeno);
+
+            //Lze naopak najít index konkrétního výskytu znaku (prvního výskytu ve stringu)
+            Console.WriteLine(a.IndexOf("C")); //IndeOf(znak,který hledáme)
+            Console.WriteLine(a.Substring(4).IndexOf("C")); //Substring začne indexovat od konkrétního indexu, který je pak považován jako index 0
+
+            //Speciální znaky
+            Console.WriteLine("\"text v dvojitých uvozovkách\" ");
+            Console.WriteLine("\'text v jednoduchých uvozovkách\'");
+            Console.WriteLine("\\"); //dvě zpětné lomítka pro zobrazeního jednoho
+            Console.WriteLine("Text jednoho řádku \nText druhého řádku"); //lomítko \n pro nový řádek
+
+
+
+        }
+    }
+}
