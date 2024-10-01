f0cc80df3a64179804f57684589d36b4e5682aba
Jan Mottl <mottja@delta-skola.cz>
2024-10-01 12:20:44 +0200

    kk

---
+++ 02/Proc_Znamkovac/Proc_Znamkovac/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Proc_Znamkovac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej mi procenta z testu ");
+            float procenta = float.Parse(Console.ReadLine());
+
+            switch (procenta)
+            {
+                case float a when (a <= 100 && a >= 83):
+                    Console.WriteLine("Získal jsi známku 1!");
+                    break;
+                case float b when (b < 83 && b >= 67):
+                    Console.WriteLine("Získal jsi známku 2!");
+                    break;
+                case float c when (c < 67 && c >= 50):
+                    Console.WriteLine("Získal jsi známku 3!");
+                    break;
+                case float d when (d < 50 && d >= 33):
+                    Console.WriteLine("Získal jsi známku 4!");
+                    break;
+                case float d when (d < 33 && d >= 0):
+                    Console.WriteLine("Získal jsi známku 5!");
+                    break;
+                default:
+                    Console.WriteLine("Špatně zadaná procenta!");
+                    break;
+            }
+        }
+    }
+}
