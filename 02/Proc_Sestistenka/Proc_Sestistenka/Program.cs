b2c99ce22b190c612497770114f09e245688cf55
Jan Mottl <mottja@delta-skola.cz>
2024-10-14 10:18:14 +0200

    kk

---
+++ 02/Proc_Sestistenka/Proc_Sestistenka/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Proc_Sestistenka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random cislo = new Random();
+
+            int hod = cislo.Next(1, 7); //1-6
+
+            switch (hod)
+            {
+                case 1:
+                    Console.WriteLine("Padla jednička!");
+                    break;
+                case 2:
+                    Console.WriteLine("Padla dvojka!");
+                    break;
+                case 3:
+                    Console.WriteLine("Padla trojka!");
+                    break;
+                case 4:
+                    Console.WriteLine("Padla čtyřka!");
+                    break;
+                case 5:
+                    Console.WriteLine("Padla pětka!");
+                    break;
+                case 6:
+                    Console.WriteLine("Padla šestka!");
+                    break;
+            }
+        }
+    }
+}
