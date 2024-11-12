96e6066b03373679ad83441e0ca9817197c2c435
Jan Mottl <mottja@delta-skola.cz>
2024-11-12 11:55:18 +0100

    kk

---
+++ 04/Proc_Obracene_slovo/Proc_Obracene_slovo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Proc_Obracene_slovo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string slovo = "les pes kaluž";
+
+            for (int i = slovo.Length-1; i >= 0; i--)
+            {
+                Console.Write(slovo[i]);
+            }
+        }
+    }
+}
