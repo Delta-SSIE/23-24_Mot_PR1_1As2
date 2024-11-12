d69d08901bc1b2b59ad941270ad22d22ba43ecb9
Jan Mottl <mottja@delta-skola.cz>
2024-11-12 12:51:52 +0100

    kkk

---
+++ 04/Proc_Jiz_rady/Proc_Jiz_rady/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Proc_Jiz_rady
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minuty = 342;
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"{minuty/60}:{minuty%60} {i+1}.Autobus");
+                minuty += 13;
+            }
+        }
+    }
+}
---
+++ 04/Proc_sifrovaciStroj_basic/Proc_sifrovaciStroj_basic/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Proc_sifrovaciStroj_basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej mi text na zašifrování");
+            string text = Console.ReadLine();
+            string zasifrovano = "";
+            string desifrovano = "";
+
+            for (int i = text.Length-1; i >= 0; i--)
+            {
+                switch (text[i])
+                {
+                    case 'a':
+                        zasifrovano += '#';
+                        break;
+                    case 'o':
+                        zasifrovano += '$';
+                        break;
+                    case 'i':
+                        zasifrovano += '&';
+                        break;
+                    default:
+                        zasifrovano += text[i];
+                        break;
+                }
+            }
+
+            Console.WriteLine(zasifrovano);
+
+            for (int i = zasifrovano.Length - 1; i >= 0; i--)
+            {
+                switch (zasifrovano[i])
+                {
+                    case '#':
+                        desifrovano += 'a';
+                        break;
+                    case '$':
+                        desifrovano += 'o';
+                        break;
+                    case '&':
+                        desifrovano += 'i';
+                        break;
+                    default:
+                        desifrovano += zasifrovano[i];
+                        break;
+                }
+            }
+            Console.WriteLine(desifrovano);
+        }
+    }
+}
