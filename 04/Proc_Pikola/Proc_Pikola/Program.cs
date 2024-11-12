30a334ec09db97f056e90f3a40c1ec12e0bc8db4
Jan Mottl <mottja@delta-skola.cz>
2024-11-12 11:45:37 +0100

    kk

---
+++ 04/Proc_Pikola/Proc_Pikola/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Proc_Pikola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i < 201; i+=10)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Už jdu!");
+        }
+    }
+}
---
+++ 04/Proc_Sudy_cisla/Proc_Sudy_cisla/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Proc_Sudy_cisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sude = 0;
+            int cislo = 0;
+
+            for (int i = 1; i < 11; i++)
+            {
+                do
+                {
+                    Console.WriteLine($"Zadej číslo na {i}.pozici!");
+                } while (!int.TryParse(Console.ReadLine(), out cislo));
+
+                if(i % 2 == 0)
+                {
+                    sude += cislo;
+                }
+            }
+
+            Console.WriteLine($"Součet čísel na sudých pozicích je {sude}");
+        }
+    }
+}
