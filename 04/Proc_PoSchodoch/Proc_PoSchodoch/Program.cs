b85e8d89e909717b719e32f8906481b2bfdac697
Jan Mottl <mottja@delta-skola.cz>
2024-11-18 10:18:42 +0100

    kk

---
+++ 04/Proc_PoSchodoch/Proc_PoSchodoch/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Proc_PoSchodoch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int patra = 0;
+            Console.WriteLine("Zadej mi počet pater?");
+            while(!int.TryParse(Console.ReadLine(),out patra))
+            {
+                Console.WriteLine("Zadej číslo!");
+            }
+
+            for (int i = 0; i < patra; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
