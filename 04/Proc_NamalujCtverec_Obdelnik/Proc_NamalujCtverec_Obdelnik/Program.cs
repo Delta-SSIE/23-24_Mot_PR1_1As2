aeaaaeba13a702d2b1b7cf51398234aa05bc0dfc
Jan Mottl <mottja@delta-skola.cz>
2024-11-18 10:09:15 +0100

    kk

---
+++ 04/Proc_NamalujCtverec_Obdelnik/Proc_NamalujCtverec_Obdelnik/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Proc_NamalujCtverec_Obdelnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Čtverec
+            int delka = 0;
+            Console.WriteLine("Zadej mi délku strany čtverce?");
+            while (!int.TryParse(Console.ReadLine(), out delka))
+            {
+                Console.WriteLine("Zadej číslo!");
+            }
+
+            for (int i = 0; i < delka; i++)
+            {
+                for (int j = 0; j < delka; j++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+
+            int delkaObA = 0;
+            int delkaObB = 0;
+            Console.WriteLine("Zadej mi délku stranu obdelníku A?");
+            while (!int.TryParse(Console.ReadLine(), out delkaObA))
+            {
+                Console.WriteLine("Zadej číslo!");
+            }
+            Console.WriteLine("Zadej mi délku stranu obdelníku B?");
+            while (!int.TryParse(Console.ReadLine(), out delkaObB))
+            {
+                Console.WriteLine("Zadej číslo!");
+            }
+
+            for (int i = 0; i < delkaObB; i++)
+            {
+                for (int j = 0; j < delkaObA; j++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
