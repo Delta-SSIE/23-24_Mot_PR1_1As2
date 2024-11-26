4eb43700279385c38aa4cd7eb4d72010c6675528
Jan Mottl <mottja@delta-skola.cz>
2024-11-26 12:41:48 +0100

    kk

--- 04/Ceasarova_sifra/Ceasarova_sifra/Program.cs
+++ 04/Ceasarova_sifra/Ceasarova_sifra/Program.cs
@@ -57,7 +57,7 @@
             Console.WriteLine(zas_text);
             */
 
-            //Pokročilá verze
+            /*Pokročilá verze
             string text = "dyrvo to woqk dktxk jzbkfk my xsuny xocws jscukd";
             string des_text = "";
             int posun = 10;
@@ -105,6 +105,66 @@
 
             }
             Console.WriteLine(zas_text);
+
+            */
+
+            //Neznámý posun
+            string text = "bpm eig gwcz lil twwsml ib qb bpqa eibkp eia gwcz jqzbpzqopb pml jm liuvml qn ivg atwxma owvvi xcb bpmqz ozmiag gmttwe pivla wv pqa jwga jqzbpzqopb aw pm pql qb qv bpm wvm xtikm pm svme pm kwctl pqlm awumbpqvo pqa iaa nqdm twvo gmiza pm ewzm bpqa eibkp cx pqa iaa bpmv epmv pm lqml wn lgamvbmzg pm oidm um bpm eibkp q pql bpqa cvkwunwzbijtm xqmkm wn umbit cx ug iaa nwz bew gmiza bpmv inbmz amdmv gmiza q eia amvb pwum bw ug niuqtg ivl vwe tqbbtm uiv q oqdm bpm eibkp bw gwc";
+            string des_text = "";
+            string slovo = "yellow";
+            bool b = false;
+
+            for (int j = 1; j < 21; j++)
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    int cislo_znaku = text[i];
+                    if (cislo_znaku == 32) // 32 je mezera
+                    {
+                        des_text += text[i];
+                    }
+                    else
+                    {
+                        cislo_znaku -= j;
+                        if (cislo_znaku < 97)
+                        {
+                            cislo_znaku += 26;
+                        }
+                        char novy_znak = (char)cislo_znaku;
+                        des_text += novy_znak;
+                    }
+
+                }
+                string nac_slovo = ""; 
+                
+                for (int i = 0; i < des_text.Length; i++)
+                {
+                    int cislo_znaku = text[i];
+                    if (cislo_znaku == 32) // 32 je mezera
+                    {
+                        if (nac_slovo == slovo)
+                        {
+                            b = true;
+                            break;
+                        }
+                        else
+                        {
+                            nac_slovo = "";
+                        }
+                    }
+                    else
+                    {
+                        nac_slovo += des_text[i];
+                    }   
+                }
+                if (b)
+                {
+                    break;
+                }
+                des_text = "";
+            }
+            Console.WriteLine(des_text);
+
         }
     }
 }
--- 04/Ceasarova_sifra/Ceasarova_sifra/obj/Debug/net8.0/Ceasarova_sifra.AssemblyInfo.cs
+++ 04/Ceasarova_sifra/Ceasarova_sifra/obj/Debug/net8.0/Ceasarova_sifra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ceasarova_sifra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b603f501da7cb34bb8adc6d591f274897c3a033c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+536e341a73d1d6c4a65cd0e2122bdb0886bb1a36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ceasarova_sifra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ceasarova_sifra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
