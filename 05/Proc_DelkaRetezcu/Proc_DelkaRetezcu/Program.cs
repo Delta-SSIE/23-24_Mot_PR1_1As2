0420f4c2ab228acfb42333775e8ccaf06791226b
Jan Mottl <mottja@delta-skola.cz>
2024-12-02 10:47:57 +0100

    kk

---
+++ 05/Proc_DelkaRetezcu/Proc_DelkaRetezcu/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Proc_DelkaRetezcu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] slova = new string[5]; //deklarace pole o délce 5
+
+            for (int i = 0; i < slova.Length; i++)
+            {
+                Console.WriteLine($"{i+1}. slovo: ");
+                slova[i] = Console.ReadLine();
+            }
+
+            Console.Write("Zde vypisuji slova, která mají alespoň 6 znaků: ");
+            foreach (string slovo in slova)
+            {
+                if(slovo.Length >= 6)
+                {
+                    Console.Write(slovo + ", ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 05/Proc_NajdiPrvek/Proc_NajdiPrvek/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Proc_NajdiPrvek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla = new int[] { -11, 0, 5, -12, 13, 24, 19, 13, -17 };
+            int cislo = 0;
+            int index = -1;
+            Console.WriteLine("Zadej mi číslo, které budeš v poli hledat!");
+            while(!int.TryParse(Console.ReadLine(),out cislo))
+            {
+                Console.WriteLine("Zadej číslo!");
+            }
+
+            for (int i = 0; i < cisla.Length; i++)
+            {
+                if(cislo == cisla[i])
+                {
+                    index = i;
+                    break; //vyskočení z cyklu
+                }
+            }
+            if(index != -1)
+            {
+                Console.WriteLine($"{cislo} se nachází na indexu {index}.");
+            } else
+            {
+                Console.WriteLine($"{cislo} se v poli nenachází.");
+            }
+            
+        }
+    }
+}
