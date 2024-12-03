d595ad523a4f5de533a870e9c50d5c3af5d4e1f3
Jan Mottl <mottja@delta-skola.cz>
2024-12-03 12:08:58 +0100

    kk

---
+++ 05/Proc_DruhaPismena/Proc_DruhaPismena/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Proc_DruhaPismena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Statný valach ojnici zničil. Révou zhrdla žena ostrovana Claudia Pompeia. Ozvučení hnědé místnosti okamžitě zvýraznit! Běžný statkář to zvládne. Japonec Anihito rád označoval čárami. Asi sto pět kravských žaludků";
+            string[] slova = text.Split(' '); //oddělovač pro rozdělení prvků v poli je mezera
+
+            foreach (string slovo in slova)
+            {
+                Console.Write(slovo[1]);
+            }
+
+            
+        }
+    }
+}
