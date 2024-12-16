2f2dd178b3dde4dcead31ae335740b2daad6f4bd
Jan Mottl <mottja@delta-skola.cz>
2024-12-16 10:52:49 +0100

    kk

---
+++ 06/Proc_NejvetsiCislo/Proc_NejvetsiCislo/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Proc_NejvetsiCislo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cislo1 = 5.3;
+            double cislo2 = 4.6;
+            double cislo3 = 6.3;
+            double maximum = Maximum(cislo1, cislo2, cislo3);
+            Console.WriteLine($"Největší ze zadaných čísel je {maximum}.");
+        }
+
+        static double Maximum(double a, double b, double c)
+        {
+            /* Přes funkci Max
+            double max1 = Math.Max(a, b);
+            max1 = Math.Max(max1, c);
+            return max1;
+            */
+
+            if(a > b)
+            {
+                if( a > c)
+                {
+                    return a;
+                } else
+                {
+                    return c;
+                }
+            } else
+            {
+                if(b > c)
+                {
+                    return b;
+                } else
+                {
+                    return c;
+                }
+            }
+        }
+    }
+}
