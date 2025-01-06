55ce0f2c947c8da33b58627cc3dd30c5d29959ef
Jan Mottl <mottja@delta-skola.cz>
2025-01-06 10:06:00 +0100

    kk

---
+++ 06/Proc_PodLimitem/Proc_PodLimitem/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Proc_PodLimitem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] cisla = { -1.3, 2.2, 1.5, 1.4, 2.7 };
+            double[] cisla2 = { -1.6, 2.5, 3.5, 1.8, 2.2 };
+            Console.WriteLine(PodLimitem(cisla, 1.5)); //vypíše -1.3 a 1.4 
+            Console.WriteLine(PodLimitem(cisla, 1.6)); //vypíše -1.3 , 1.5 a 1.4
+            Console.WriteLine(PodLimitem(cisla2, 2));
+        }
+
+        static string PodLimitem(double[] pole, double limit)
+        {
+            string cisla = " ";
+            for (int i = 0; i < pole.Length; i++)
+            {
+                if (pole[i] < limit)
+                {
+                    cisla += pole[i] + ", ";
+                }
+            }
+            return cisla;
+        }
+    }
+}
