163535954c779411873e9d1d6579cbc269640df3
Jan Mottl <mottja@delta-skola.cz>
2025-02-10 10:56:17 +0100

    kk

---
+++ 07/Proc_PocetSousedu/Proc_PocetSousedu/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Proc_PocetSousedu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool[,] mapa =
+            {
+                { true,  true,  false, false },
+                { false, false, true,  true },
+                { false, false, false, true },
+                { false, false, true,  true },
+            };
+
+            Console.WriteLine(PocetSousedu(mapa, 0, 0)); //mělo by vypsat 1 - levý horní roh sousedí jen s jednou minou
+            Console.WriteLine(PocetSousedu(mapa, 0, 3)); //mělo by vypsat 0 - levý dolní roh s žádnou minou nesousedí
+            Console.WriteLine(PocetSousedu(mapa, 2, 2)); //mělo by vypsat 5
+
+        }
+
+        static int PocetSousedu(bool[,] mapa, int sourX, int sourY)
+        {
+            int pocetMin = 0;
+            //sem přijde váš kód
+
+            if(sourX != mapa.GetLength(1)-1)
+            {
+                if (mapa[sourX + 1, sourY] == true)
+                {
+                    pocetMin++;
+                }
+                if(sourY != 0)
+                {
+                    if (mapa[sourX + 1, sourY - 1] == true)
+                    {
+                        pocetMin++;
+                    }
+                }
+                if(sourY != mapa.GetLength(0) - 1)
+                {
+                    if (mapa[sourX + 1, sourY + 1] == true)
+                    {
+                        pocetMin++;
+                    }
+                }
+               
+            }
+            if(sourX != 0)
+            {
+                if (mapa[sourX - 1, sourY] == true)
+                {
+                    pocetMin++;
+                }
+                if (sourY != 0)
+                {
+                    if (mapa[sourX - 1, sourY - 1] == true)
+                    {
+                        pocetMin++;
+                    }
+                }
+                if (sourY != mapa.GetLength(0) - 1)
+                {
+                    if (mapa[sourX - 1, sourY + 1] == true)
+                    {
+                        pocetMin++;
+                    }
+                }
+            }
+
+            if (sourY != 0)
+            {
+                if (mapa[sourX, sourY - 1] == true)
+                {
+                    pocetMin++;
+                }
+            }
+            if (sourY != mapa.GetLength(0) - 1)
+            {
+                if (mapa[sourX, sourY + 1] == true)
+                {
+                    pocetMin++;
+                }
+            }
+            return pocetMin;
+
+        }
+    }
+}
---
+++ Opakovani_2DPole/Opakovani_2DPole/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Opakovani_2DPole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] pole = new int[3, 3];
+            int pocitadlo = 1;
+
+            //zápis čísel po řádcích
+            for (int i = 0; i < pole.GetLength(0); i++) //řádky
+            {
+                for (int j = 0; j < pole.GetLength(1); j++) //sloupce
+                {
+                    pole[i, j] = pocitadlo;
+                    Console.Write(pole[i, j] + " ");
+                    pocitadlo++;
+                }
+                Console.WriteLine();
+            }
+
+            //zápis čísel po sloupcích
+            pocitadlo = 1;
+            for (int i = 0; i < pole.GetLength(1); i++) //sloupcích
+            {
+                for (int j = 0; j < pole.GetLength(0); j++) //řádcích
+                {
+                    pole[j, i] = pocitadlo;
+                    pocitadlo++;
+                }
+            
+            }
+            for (int i = 0; i < pole.GetLength(0); i++) //řádky
+            {
+                for (int j = 0; j < pole.GetLength(1); j++) //sloupce
+                {
+                    Console.Write(pole[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
