a37fb12b60b6031b4685bf0c5f8c090aedacf3c6
Jan Mottl <mottja@delta-skola.cz>
2025-02-25 12:53:07 +0100

    kk

---
+++ 08/Proc_Pes/Proc_Pes/Pes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proc_Pes
+{
+    internal class Pes
+    {
+        //definice vlastností 
+        public string Jmeno;
+        public int Nohy;
+        public bool JeOckovany;
+
+
+        //Metoda
+        public string PredstavSe()
+        {
+            //metoda využívá vlastnosti objektů z třídy
+            string ocko = (this.JeOckovany) ? "jsem očkovaný" : "nejsem očkovaný";
+            return $"Já jsem pes, jmenuju se {this.Jmeno}, mám {this.Nohy} nohy a {ocko}.";
+        }
+    }
+}
