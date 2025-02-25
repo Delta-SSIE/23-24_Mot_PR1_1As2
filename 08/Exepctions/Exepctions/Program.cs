0a9db32dfd8450b317dbefaad39853f3c6ed9512
Jan Mottl <mottja@delta-skola.cz>
2025-02-25 11:46:09 +0100

    kk

---
+++ 08/Exepctions/Exepctions/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exepctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zkus
+            try
+            {
+                int cislo = int.Parse(Console.ReadLine());
+            } catch //pokud kod není proveden bez chyby
+            {
+                Console.WriteLine("Měl si zadat číslo!");
+            }
+
+            try
+            {
+                int cislo2 = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e) //pokud kod není proveden bez chyby
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
