48db235b8a9f4e714bb6bff0baf7c50e8b66e8d6
Jan Mottl <mottja@delta-skola.cz>
2025-03-10 10:01:32 +0100

    kk

---
+++ 08/Zapouzdreni_BankovniUcty/Zapouzdreni_BankovniUcty/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Zapouzdreni_BankovniUcty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucet mujUcet = new Ucet();
+            mujUcet.Bilance = 200;
+            mujUcet.JmenoVlastnika = "Jan Záruba";
+            int vyber = 0;
+
+            while (true)
+            {
+                Console.WriteLine($"Vlastník účtu: {mujUcet.JmenoVlastnika}");
+                Console.WriteLine($"Zůstatek na účtu: {mujUcet.Bilance}");
+                Console.WriteLine("Pro výběr napiš 1; Pro vklad napiš 2");
+                try
+                {
+                    vyber = int.Parse(Console.ReadLine());
+                } catch
+                {
+                    Console.WriteLine("Zadaná jiná hodnota než číslo");
+                }
+                double castka = 0;
+                switch (vyber)
+                {
+                    case 1:
+                        Console.WriteLine("Kolik chceš vybrat");
+                        castka = double.Parse(Console.ReadLine());
+                        mujUcet.Bilance -= castka;
+                        break;
+                    case 2:
+                        Console.WriteLine("Kolik chceš vložit");
+                        castka = double.Parse(Console.ReadLine());
+                        mujUcet.Bilance += castka;
+                        break;
+                    default:
+                        Console.WriteLine("Tato možnost neexistuje");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ 08/Zapouzdreni_BankovniUcty/Zapouzdreni_BankovniUcty/Ucet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zapouzdreni_BankovniUcty
+{
+    internal class Ucet
+    {
+        public string JmenoVlastnika;
+        public double Bilance;
+    }
+}
