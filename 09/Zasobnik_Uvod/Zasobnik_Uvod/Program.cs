ecd25b2d05a089a7315b5a06ee4786870eb3daee
Jan Mottl <mottja@delta-skola.cz>
2025-03-31 10:55:27 +0200

    kk

---
+++ 09/Zasobnik_Uvod/Zasobnik_Uvod/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Zasobnik_Uvod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklarace zásobniku
+            Stack<string> jmena = new Stack<string>();
+
+            //přidání prvků do zásobníku
+            jmena.Push("Kamil");
+            jmena.Push("Jindra");
+            jmena.Push("Ruprecht");
+            jmena.Push("Jarmila");
+            jmena.Push("Ivana");
+
+            //průchod
+            foreach (string jmeno in jmena)
+            {
+                Console.WriteLine(jmeno);
+            }
+            Console.WriteLine("--------------");
+            //peek - ukáže se první prvek na vrcholu zásobníku
+            Console.WriteLine(jmena.Peek());
+
+            //FIFO - first in, first out
+            jmena.Pop(); //vyhodí první prvek = Ivana
+            jmena.Pop(); //vyhodí nový první prvek = Jarmila
+            Console.WriteLine("--------------");
+            //peek - ukáže se první prvek na vrcholu zásobníku
+            Console.WriteLine(jmena.Peek()); //Ruprecht
+
+
+        }
+    }
+}
