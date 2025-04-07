4ccbc75e54f98375220c457a666a9548d8d61834
Jan Mottl <mottja@delta-skola.cz>
2025-04-07 11:22:27 +0200

    kk

---
+++ 09/json_uvod/json_uvod/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+
+namespace json_uvod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student studentA = new Student
+            {
+                id = 5,
+                first_name = "John",
+                last_name = "Fisher",
+                email = "fish@fish.fish"
+            };
+
+            string json = JsonSerializer.Serialize(studentA);
+            Console.WriteLine(json);
+
+            Student studentB = JsonSerializer.Deserialize<Student>(json);
+            Console.WriteLine(studentB.first_name + " " + studentB.last_name);
+
+
+            //tahaní z zdrojového souboru
+    
+            Student[] studenti = JsonSerializer.Deserialize<Student[]>(File.ReadAllText("data.json"));
+
+            for (int i = 0; i < studenti.Length; i++)
+            {
+                Console.WriteLine(studenti[i].first_name);
+            }
+
+           
+
+        }
+    }
+}
---
+++ 09/json_uvod/json_uvod/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace json_uvod
+{
+    internal class Student
+    {
+        public int id { get; set; }
+        public string first_name {get; set;}
+        public string last_name { get; set; }
+        public string email { get; set; }
+    }
+}
